a2f8de70ac3368ea9d932c369040a9c17b06159f
Mitchell Borchers <30349201+borchr27@users.noreply.github.com>
2022-10-27 10:43:23 +0200

    make the site spider, scrape homepage text

--- practice/scrapy_tut/tutorial/tutorial/spiders/site_spider.py
+++ practice/scrapy_tut/tutorial/tutorial/spiders/site_spider.py
@@ -5,7 +5,7 @@ class SiteSpider(scrapy.Spider):
     name = 'site'
     def __init__(self, *args, **kwargs):
         super(SiteSpider).__init__(*args, **kwargs)
-        self.start_urls = ['https://www.livedale.co.uk/', 'http://mcgeoughsnisa.co.uk/']
+        self.start_urls = ['https://www.livedale.co.uk/', 'http://mcgeoughsnisa.co.uk/', 'http://bluebadgerwi.com/', 'https://m.facebook.com/tostoria', 'https://parcelbroker.co.uk/']
 
     def start_requests(self):
         for url in self.start_urls:
