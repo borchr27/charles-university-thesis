07efacac92d09c2d5b92534b3b45dd632f27df41
Mitchell Borchers <30349201+borchr27@users.noreply.github.com>
2022-11-07 18:19:48 +0100

    add sites to scrape into file, csv format

--- practice/scrapy_tut/tutorial/tutorial/spiders/site_spider.py
+++ practice/scrapy_tut/tutorial/tutorial/spiders/site_spider.py
@@ -1,11 +1,18 @@
 import scrapy
 from tutorial.items import TutorialItem
+import csv
+import pathlib
+import itertools
 
 class SiteSpider(scrapy.Spider):
     name = 'site'
     def __init__(self, *args, **kwargs):
         super(SiteSpider).__init__(*args, **kwargs)
-        self.start_urls = ['https://www.livedale.co.uk/', 'http://mcgeoughsnisa.co.uk/', 'http://bluebadgerwi.com/', 'https://m.facebook.com/tostoria', 'https://parcelbroker.co.uk/']
+        p = pathlib.Path.cwd() / 'sites.csv'
+        self.logger.warning('IS THE PATH TO OPEN site.csv CORRECT?...', p)
+        with p.open() as file:
+            data = list(itertools.chain.from_iterable(csv.reader(file)))
+        self.start_urls = [url.strip() for url in data]
 
     def start_requests(self):
         for url in self.start_urls:
