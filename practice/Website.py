14fea6edd03057026f9b15728444d5724646368b
Mitchell Borchers <30349201+borchr27@users.noreply.github.com>
2023-02-19 23:03:10 +0100

    same as previous but readding the files

---
+++ practice/Website.py
@@ -0,0 +1,10 @@
+
+
+class Website:
+    def __init__(self, name, i_url, v_url, text, category, tags):
+        self.name = name
+        self.i_url = i_url
+        self.v_url = v_url
+        self.text = text
+        self.category = category
+        self.tags = tags
